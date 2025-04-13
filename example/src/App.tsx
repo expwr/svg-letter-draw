0e8d26fe9be36177f7d7a9e977f9e5862da63753
Noah Diana <noah.diana10312005@gmail.com>
2025-04-12 21:47:05 -0400

    Update App.tsx

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,19 +1,30 @@
-import React from "react";
 import { Draw } from "../../src";
 // import { Draw } from "svg-letter-draw";
 import BarlowSemiCondensed from "./fonts/BarlowSemiCondensed-Black.ttf";
+import BarlowSemiCondensedRegular from "./fonts/BarlowSemiCondensed-Regular.ttf";
+import SpecialGothic from "./fonts/SpecialGothicExpandedOne-Regular.ttf";
+import DancingScript from "./fonts/DancingScript-VariableFont_wght.ttf";
+import RobotoSlab from "./fonts/RobotoSlab-VariableFont_wght.ttf";
 
 function App() {
   return (
     <div style={{ margin: "2rem" }}>
       <h1>Demo for svg-letter-draw Component</h1>
-      <Draw
-        fontUrl={BarlowSemiCondensed}
-        text="Noah Diana"
-        totalAnimationDuration={3.5}
-        animationDelayMode="sequential"
-        letterSpacing={2}
-      />
+      <div style={{ width: "1200px" }}>
+        <Draw
+          fontUrl={SpecialGothic}
+          text="svg-letter-draw"
+          letterSpacing={2}
+          letterAnimationDuration={2.0}
+          letterDelay={0.1}
+          strokeWidth={3}
+          lineColor={["#FF5733", "#FFD700", "#32CD32", "#1E90FF"]}
+          fillColor={["#FFC0CB", "#98FB98", "#ADD8E6", "#E6E6FA"]}
+          fillDirection="bottom"
+          fillAnimationType="fade"
+          fillDrawDuration={2}
+        />
+      </div>
     </div>
   );
 }
